5502ad44a964dcb807b39f8e74368eebafe746ad
sid23489 <siddharth234890@gmail.com>
2025-09-28 12:47:04 +0530

    first commit

---
+++ src/main/java/com/siddharth/helios/HeliosApplication.java
@@ -0,0 +1,13 @@
+package com.siddharth.helios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeliosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siddharth/helios/HeliosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddharth.helios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
